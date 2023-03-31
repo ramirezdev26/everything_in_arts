915fe163bbb55262298211ab97baf925cf23af1a
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:58:33 -0500

    Adding the extends for the MongoDBRepository in driven adapter

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/everything_in_arts/mongo/MongoDBRepository.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/everything_in_arts/mongo/MongoDBRepository.java
@@ -1,7 +1,8 @@
 package co.com.everything_in_arts.mongo;
 
+import co.com.everything_in_arts.mongo.data.ItemData;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 
-public interface MongoDBRepository extends ReactiveMongoRepository<Object/* change for adapter model */, String>, ReactiveQueryByExampleExecutor<Object/* change for adapter model */> {
+public interface MongoDBRepository extends ReactiveMongoRepository<ItemData, String> {
 }
