a7ac75a4a1264790467d88b5f839cfeaea358d32
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:55:28 -0500

    Created the GetAll Use Case for Item

--- domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemgetall/ItemGetAllUseCase.java
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemgetall/ItemGetAllUseCase.java
@@ -1,6 +1,21 @@
 package co.com.everything_in_arts.usecase.itemgetall;
 
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.model.item.gateways.ItemRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-@RequiredArgsConstructor
-public class ItemGetAllUseCase {
+import reactor.core.publisher.Flux;
+
+import java.util.function.Supplier;
+
+@AllArgsConstructor
+public class ItemGetAllUseCase implements Supplier<Flux<Item>> {
+
+    private final ItemRepository repository;
+
+
+    @Override
+    public Flux<Item> get() {
+        return repository.getAllItems();
+    }
 }
