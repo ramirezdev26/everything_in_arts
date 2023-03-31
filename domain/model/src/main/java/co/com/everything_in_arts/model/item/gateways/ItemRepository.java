915c0cb5cde3848564462b1e45ad895c48db6e6f
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:56:28 -0500

    Created Item Repository interface with all the CRUD methods

--- domain/model/src/main/java/co/com/everything_in_arts/model/item/gateways/ItemRepository.java
+++ domain/model/src/main/java/co/com/everything_in_arts/model/item/gateways/ItemRepository.java
@@ -1,4 +1,15 @@
 package co.com.everything_in_arts.model.item.gateways;
 
+import co.com.everything_in_arts.model.item.Item;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ItemRepository {
+
+    Flux<Item> getAllItems();
+    Mono<Item> getItemById(String id);
+    Mono<Item> saveItem(Item item);
+    Mono<Item> updateItem(String id, Item item);
+    Mono<Void> deleteItem(String id);
+
 }
