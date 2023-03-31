f4ff8d2fbd19cb00c8998be6e821cb0539f65a4e
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:55:51 -0500

    Created GetById Use Case for Item

--- domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemgetbyid/ItemGetByIdUseCase.java
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemgetbyid/ItemGetByIdUseCase.java
@@ -1,6 +1,20 @@
 package co.com.everything_in_arts.usecase.itemgetbyid;
 
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.model.item.gateways.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class ItemGetByIdUseCase {
+public class ItemGetByIdUseCase implements Function<String, Mono<Item>> {
+
+    private final ItemRepository repository;
+
+    public Mono<Item> apply(String id){
+        return repository.getItemById(id);
+    }
+
+
 }
