e8b8487d4e029b8cff06254de33a2bfae5fc61de
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:57:06 -0500

    Created Save Use Case for Item entity

--- domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemsave/ItemSaveUseCase.java
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemsave/ItemSaveUseCase.java
@@ -1,6 +1,19 @@
 package co.com.everything_in_arts.usecase.itemsave;
 
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.model.item.gateways.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class ItemSaveUseCase {
+public class ItemSaveUseCase implements Function<Item, Mono<Item>> {
+
+    private final ItemRepository repository;
+
+    @Override
+    public Mono<Item> apply(Item item) {
+        return repository.saveItem(item);
+    }
 }
