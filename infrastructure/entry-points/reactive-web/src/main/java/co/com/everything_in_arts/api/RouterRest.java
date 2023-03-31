205a1018bf21bcefaf3118e162f4da17ecc2e810
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 15:00:00 -0500

    Implementation of entry point with the responses and the Swagger documentation

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/everything_in_arts/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/everything_in_arts/api/RouterRest.java
@@ -1,20 +1,139 @@
 package co.com.everything_in_arts.api;
 
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.usecase.itemdelete.ItemDeleteUseCase;
+import co.com.everything_in_arts.usecase.itemgetall.ItemGetAllUseCase;
+import co.com.everything_in_arts.usecase.itemgetbyid.ItemGetByIdUseCase;
+import co.com.everything_in_arts.usecase.itemsave.ItemSaveUseCase;
+import co.com.everything_in_arts.usecase.itemupdate.ItemUpdateUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterRest {
     @Bean
-    public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-        return route(GET("/api/usecase/path"), handler::listenGETUseCase)
-                .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase)
-                .and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
+    @RouterOperation(path = "/items", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = ItemGetAllUseCase.class, method = RequestMethod.GET,
+            beanMethod = "get",
+            operation = @Operation(operationId = "getAllItems", tags = "Item usecases",
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Success",
+                                    content = @Content(schema = @Schema(implementation = Item.class))),
+                            @ApiResponse(responseCode = "204", description = "Nothing to show")
+                    }))
+    public RouterFunction<ServerResponse> getAllItems (ItemGetAllUseCase itemGetAllUseCase){
+        return route(GET("/items"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(itemGetAllUseCase.get(), Item.class))
+                        .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(throwable.getMessage()))
+        );
+    }
+
+    @Bean
+    @RouterOperation(path = "/items/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = ItemGetByIdUseCase.class,
+            method = RequestMethod.GET,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "getItemById", tags = "Items usecases",
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Success",
+                                    content = @Content (schema = @Schema(implementation = Item.class))),
+                            @ApiResponse(responseCode = "404", description = "Not Found")
+                    }))
+    public RouterFunction<ServerResponse> getItemById(ItemGetByIdUseCase itemGetByIdUseCase){
+        return route(GET("items/{id}"),
+                request -> itemGetByIdUseCase.apply(request.pathVariable("id"))
+                        .flatMap(item -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(item))
+                        .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(throwable.getMessage()))
+        );
+    }
+
+    @Bean
+    @RouterOperation(path = "/items", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = ItemSaveUseCase.class, method = RequestMethod.POST,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "saveItem", tags = "Items usecases",
+                    responses = {
+                            @ApiResponse(responseCode = "201", description = "Success", content = @Content(schema = @Schema(implementation = Item.class))),
+                            @ApiResponse(responseCode = "406", description = "Not acceptable. Try again")},
+                    requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = Item.class)))))
+
+    public RouterFunction<ServerResponse> saveItem (ItemSaveUseCase itemSaveUseCase){
+        return route(POST("/items").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(Item.class)
+                        .flatMap(item -> itemSaveUseCase.apply(item)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.BAD_REQUEST).bodyValue(throwable.getMessage())))
+        );
+    }
+
+    @Bean
+    @RouterOperation(path = "/items/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = ItemUpdateUseCase.class, method = RequestMethod.PUT, beanMethod = "apply",
+            operation = @Operation(operationId = "updateItem", tags = "Items usecases",
+                    responses = {
+                            @ApiResponse(responseCode = "202", description = "Accepted", content = @Content(schema = @Schema(implementation = Item.class))),
+                            @ApiResponse(responseCode = "406", description = "Not acceptable. Try again")},
+                    requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = Item.class)))))
+    public RouterFunction<ServerResponse> updateItem(ItemUpdateUseCase itemUpdateUseCase){
+        return route(PUT("/items/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(Item.class)
+                        .flatMap(item -> itemUpdateUseCase.apply(request.pathVariable("id"),
+                                        item)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.BAD_REQUEST)
+                                        .bodyValue(throwable.getMessage()))
+                        )
+        );
+    }
+
+    @Bean
+    @RouterOperation(path = "/items/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = ItemDeleteUseCase.class, method = RequestMethod.DELETE,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "deleteFlowerById", tags = "Flowers usecases",
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Success",
+                                    content = @Content (schema = @Schema(implementation = Item.class))),
+                            @ApiResponse(responseCode = "404", description = "Item Not Found")
+                    }))
+    public RouterFunction<ServerResponse> deleteItem (ItemDeleteUseCase itemDeleteUseCase){
+        return route(DELETE("/items/{id}"),
+                request -> itemDeleteUseCase.apply(request.pathVariable("id"))
+                        .thenReturn(ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue("Item deleted"))
+                        .flatMap(serverResponseMono -> serverResponseMono)
+                        .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(throwable.getMessage()))
+        );
     }
 }
