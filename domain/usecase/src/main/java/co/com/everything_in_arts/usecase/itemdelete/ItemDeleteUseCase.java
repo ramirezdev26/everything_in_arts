c489350d3024ef056d7ea36f96b5b567d3b9908d
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:55:04 -0500

    Created the Delete Use Case for Item

--- domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemdelete/ItemDeleteUseCase.java
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemdelete/ItemDeleteUseCase.java
@@ -1,6 +1,22 @@
 package co.com.everything_in_arts.usecase.itemdelete;
 
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.model.item.gateways.ItemRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class ItemDeleteUseCase {
+public class ItemDeleteUseCase implements Function<String, Mono<Void>> {
+
+    private final ItemRepository repository;
+
+
+    @Override
+    public Mono<Void> apply(String s) {
+        return repository.deleteItem(s);
+    }
 }
