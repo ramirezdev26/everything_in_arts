cc26e12d98226988e3d0b70dd9a001da321423ec
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-31 14:57:23 -0500

    Created Update Use Case for Item entity

--- domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemupdate/ItemUpdateUseCase.java
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/itemupdate/ItemUpdateUseCase.java
@@ -1,6 +1,19 @@
 package co.com.everything_in_arts.usecase.itemupdate;
 
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.model.item.gateways.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
+
 @RequiredArgsConstructor
-public class ItemUpdateUseCase {
+public class ItemUpdateUseCase implements BiFunction<String, Item, Mono<Item>> {
+
+    private final ItemRepository repository;
+
+    @Override
+    public Mono<Item> apply(String id, Item item) {
+        return repository.updateItem(id, item);
+    }
 }
