1f759812553371b65bb9a7977408d02dd9342029
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-03 16:52:33 -0500

    Added Get by id use case for invoice

---
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/invoicegetbyid/InvoiceGetByIdUseCase.java
@@ -0,0 +1,20 @@
+package co.com.everything_in_arts.usecase.invoicegetbyid;
+
+import co.com.everything_in_arts.model.invoice.Invoice;
+import co.com.everything_in_arts.model.invoice.gateways.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class InvoiceGetByIdUseCase implements Function<String, Mono<Invoice>> {
+
+    private final InvoiceRepository repository;
+
+    @Override
+    public Mono<Invoice> apply(String id) {
+        return repository.getInvoiceById(id);
+    }
+}
+
