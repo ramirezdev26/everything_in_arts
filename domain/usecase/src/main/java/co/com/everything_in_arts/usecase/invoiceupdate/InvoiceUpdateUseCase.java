d4811ef89ed8a7fe33fc8152eeda0dae6d5b8967
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-03 16:53:11 -0500

    Added Update use case for invoice

---
+++ domain/usecase/src/main/java/co/com/everything_in_arts/usecase/invoiceupdate/InvoiceUpdateUseCase.java
@@ -0,0 +1,21 @@
+package co.com.everything_in_arts.usecase.invoiceupdate;
+
+import co.com.everything_in_arts.model.invoice.Invoice;
+import co.com.everything_in_arts.model.invoice.gateways.InvoiceRepository;
+import co.com.everything_in_arts.model.item.Item;
+import co.com.everything_in_arts.model.item.gateways.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
+
+@RequiredArgsConstructor
+public class InvoiceUpdateUseCase implements BiFunction<String, Invoice, Mono<Invoice>> {
+
+    private final InvoiceRepository repository;
+
+    @Override
+    public Mono<Invoice> apply(String id, Invoice invoice) {
+        return repository.updateInvoice(id, invoice);
+    }
+}
